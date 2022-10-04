2d86b68b9fdbfba64a7a74b573ba7b18e63f2c12
Ksenya <113295790+ksblv@users.noreply.github.com>
2022-10-04 14:21:18 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,32 @@
+import telebot
+import random
+from telebot import types
+
+# Загружаем список поговорок
+file = open('/facts.txt', 'r', encoding='UTF-8')
+facts  = file.read().split('\n')
+file.close()
+# Создаем бота
+bot = telebot.TeleBot('5681197522:AAG18F0ArwMg2oIKjJB2gm0EyVHlrwhRXJQ')
+# Команда start
+@bot.message_handler(commands=["start"])
+def start(message, res=False):
+        # Добавляем две кнопки
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Факт")
+        item2=types.KeyboardButton("Связаться со мной")
+        markup.add(item1, item2)
+        bot.send_message(message.chat.id, 'Привет! Нажми: \nФакт для получения интересного факта о концерне BMW',  reply_markup=markup)
+            
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    # Если юзер прислал 1, выдаем ему случайный факт
+    if message.text.strip() == 'Факт' :
+            answer = random.choice(facts)
+    if message.text.strip() == 'Связаться со мной' :
+            answer = 't.me/ksblv'
+    # Отсылаем юзеру сообщение в его чат
+    bot.send_message(message.chat.id, answer)
+    
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
